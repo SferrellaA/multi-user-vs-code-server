d6eaddab9dc5fdfbd94266333640177a0f61c5b8
SferrellaA <alexander@sferrella.com>
2021-03-04 18:55:23 -0500

    filling out userList and getUser

--- handleUser.go
+++ handleUser.go
@@ -1,43 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os/user"
+)
 
-// validUser checks that a provided username is valid
-func validUser(username string) bool {
-	userList := []string{"fake"}
-	for _, u := range userList {
-		if username == u {
-			return true
+// userList returns a list of all valid users
+func userList() (users []string) {
+	// Check what users folders are in the home folder
+	items, err := ioutil.ReadDir(homeFolder)
+	if nil != err {
+		return
+	}
+
+	// Accumulate folder names that are also valid users
+	for _, i := range items {
+		if !i.IsDir() {
+			continue
+		}
+		if _, _, err := getUser(i.Name()); nil == err {
+			users = append(users, i.Name())
 		}
 	}
-	return false
+
+	return // the list of valid users with folders
 }
 
-func getUserIDs(username string) (int, int) {
-	return 1000, 1000
+// getUser returns a specified user's uid and gid, or an error if not found
+func getUser(username string) (uid string, gid string, err error) {
+	u, err := user.Lookup(username)
+	if nil == err {
+		uid = u.Uid
+		gid = u.Gid
+	}
+	return
 }
 
-func getUserHash(username string) string {
-	return "AAAAAAAAAAA"
+// getUserHash returns the password hash of the specified user
+func getUserHash(username string) (hash string, err error) {
+	return "AAAAAAAAAAA", nil
 }
 
-func getUserPort(usrname string) int {
-	return 8443
+// getUserPort returns the port a specified user's environment will run on
+func getUserPort(username string) (port int, err error) {
+	return 8443, nil
 }
 
 // generateCommand produces the command for the user's environment
-func generateCommand(username string) string {
-	puid, pgid := getUserIDs(username)
-	hash := getUserHash(username)
-	port := getUserPort(username)
+func generateCommand(username string) (command string, err error) {
+	puid, pgid, err := getUser(username)
+	if nil != err {
+		return
+	}
 
-	command := "docker run -d " +
+	hash, err := getUserHash(username)
+	if nil != err {
+		return
+	}
+
+	port, err := getUserPort(username)
+	if nil != err {
+		return
+	}
+
+	command = "docker run -d " +
 		fmt.Sprintf("--name=code-server-%s ", username) +
-		fmt.Sprintf("-e PUID=%d ", puid) +
-		fmt.Sprintf("-e PGID=%d ", pgid) +
+		fmt.Sprintf("-e PUID=%s ", puid) +
+		fmt.Sprintf("-e PGID=%s ", pgid) +
 		fmt.Sprintf("-e TZ=Europe/London ") +
 		fmt.Sprintf("-e HASHED_PASSWORD=%s ", hash) +
 		fmt.Sprintf("-p %d:8443", port)
 
-	return command
+	return
 }
--- main.go
+++ main.go
@@ -7,25 +7,40 @@ import (
 	"strings"
 )
 
+// https://hub.docker.com/r/linuxserver/code-server
+var homeFolder = "/Users"
+
+// handleMain handles /, a list of valid user links
 func handleMain(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "the main page")
+	html := "The main page<br>"
+
+	for _, user := range userList() {
+		html += fmt.Sprintf("<a href=\"/u/%s\">%s</a>", user, user)
+	}
+
+	fmt.Fprintf(w, html)
 }
 
+// handleAdmin will handle user account management
 func handleAdmin(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "the admin page")
 }
 
+// handleUser handles a single user's vs code instance
 func handleUser(w http.ResponseWriter, r *http.Request) {
 	// Parse out the user from the path
 	user := strings.TrimPrefix(r.URL.Path, "/u/")
 
 	// Check that the user actually exists
-	if !validUser(user) {
+	if _, _, err := getUser(user); nil != err {
 		http.Redirect(w, r, "/", 301)
 	}
 
 	// Generate the command for the user's environment
-	command := generateCommand(user)
+	command, err := generateCommand(user)
+	if nil != err {
+
+	}
 
 	fmt.Fprintf(w, command)
 }
